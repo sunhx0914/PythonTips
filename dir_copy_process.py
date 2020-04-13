49aecfab8f0e899caa35ddd0183f4b8465ebb4dd
sunhx0914 <15232136986@163.com>
2020-04-14 01:23:20 +0800

    update gevent

--- dir_copy_process.py
+++ dir_copy_process.py
@@ -6,10 +6,11 @@ import random
 
 def copy_file(queue, file_name,source_folder_name,  dest_folder_name):
     """copy文件到指定的路径"""
-    f_read = open(source_folder_name + "/" + file_name, "rb")
-    f_write = open(dest_folder_name + "/" + file_name, "wb")
+    # f_read = open(source_folder_name + "/" + file_name, "rb")
+    f_read = open(os.path.join(source_folder_name, file_name), "rb")
+    f_write = open(os.path.join(dest_folder_name, file_name), "wb")
     while True:
-        time.sleep(random.random())
+        # time.sleep(random.random())
         content = f_read.read(1024)  # 防止大文件
         if content:
             f_write.write(content)
@@ -17,7 +18,6 @@ def copy_file(queue, file_name,source_folder_name,  dest_folder_name):
             break
     f_read.close()
     f_write.close()
-
     # 发送已经拷贝完毕的文件名字
     queue.put(file_name)
 
@@ -25,7 +25,6 @@ def copy_file(queue, file_name,source_folder_name,  dest_folder_name):
 def main():
     # 获取要复制的文件夹
     source_folder_name = input("请输入要复制文件夹名字:")
-
     # 整理目标文件夹
     dest_folder_name = source_folder_name + "[副本]"
 
@@ -33,7 +32,7 @@ def main():
     try:
         os.mkdir(dest_folder_name)
     except:
-        pass  # 如果文件夹已经存在，那么创建会失败
+        pass  # 如果文件夹已经存在，那么创建会报异常，捕获异常，什么都不做
 
     # 获取这个文件夹中所有的普通文件名
     file_names = os.listdir(source_folder_name)
@@ -57,7 +56,7 @@ def main():
     while True:
         file_name = queue.get()
         copy_ok_num += 1
-        print("\r拷贝的进度为：%.2f %%" % (copy_ok_num*100/all_file_num), end="")
+        print("\r拷贝的进度为：%.2f %%" % (copy_ok_num/all_file_num*100), end="")
         if copy_ok_num >= all_file_num:
             break
     print()
